26515a67b2bcb59f86973a7923f7b77cb0a68088
atacadaoqueirozat-rgb <atacadaoqueirozat@gmail.com>
2025-09-06 08:16:15 -0300

    up load arquivos

---
+++ valida.js
@@ -0,0 +1,23 @@
+
+const usuarios = [
+    {
+        und:'1',
+        login: 'victor',
+        pass: 'victor',
+        linkd: "'file:///C:/Users/luiz.roberto/Desktop/trabalho/particular/imagens/1.bmp'"
+    },
+    {
+        und:'1',
+        login: 'julio',
+        pass: 'julio',
+         linkd: 'file:///C:/Users/luiz.roberto/Desktop/trabalho/particular/imagens/1.bmp'
+    },
+    {
+        und:'1',
+        login: 'admin',
+        pass: 'admin',
+         linkd: ""
+    }
+]
+
+
