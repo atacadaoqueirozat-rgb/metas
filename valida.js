b85bc39d103db3b090233d655d8dbeea7eaa407c
atacadaoqueirozat-rgb <atacadaoqueirozat@gmail.com>
2025-09-08 09:30:31 -0300

    Update valida.js

--- valida.js
+++ valida.js
@@ -3,43 +3,43 @@ const usuarios = [
 und:'21',
 login:'Alexia Alerrandra',
 pass: 'umasenha97',
-linkd: '""'
+linkd: ''
 },
 {
 und:'13',
 login:'Amanda.i',
 pass: '0410',
-linkd: '""'
+linkd: ''
 },
 {
 und:'12',
 login:'Ana karolayne do nascimento Silva ',
 pass: 'Karol10',
-linkd: '""'
+linkd: ''
 },
 {
 und:'14',
 login:'CARLOS MENEZES',
 pass: 'CARLOS25@',
-linkd: '""'
+linkd: ''
 },
 {
 und:'23',
 login:'Erick S',
 pass: '153515',
-linkd: '""'
+linkd: ''
 },
 {
 und:'14',
 login:'francielio',
 pass: '1902',
-linkd: '""'
+linkd: ''
 },
 {
 und:'13',
 login:'Francisca.g',
 pass: '2506',
-linkd: '""'
+linkd: ''
 },
 {
 und:'19',
@@ -51,61 +51,61 @@ linkd: 'https://docs.google.com/spreadsheets/d/1vpgqeObvmIC-QKVcozAiDSg2FiPfrpQ8
 und:'13',
 login:'João.costa',
 pass: '403620',
-linkd: '""'
+linkd: ''
 },
 {
 und:'15',
 login:'jose emanuel',
 pass: '051218',
-linkd: '""'
+linkd: ''
 },
 {
 und:'6',
 login:'José.w',
 pass: '@Wilton91',
-linkd: '"https://docs.google.com/spreadsheets/d/1V6f54F2QYhZgYCzd1DkYQGHqDuRdp4OT9Ki4bMH9Z1k/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1V6f54F2QYhZgYCzd1DkYQGHqDuRdp4OT9Ki4bMH9Z1k/edit?usp=sharing'
 },
 {
 und:'17',
 login:'Lucas Francisco ',
 pass: '1795',
-linkd: '""'
+linkd: ''
 },
 {
 und:'16',
 login:'Luzinete Medeiros ',
 pass: '2114',
-linkd: '""'
+linkd: ''
 },
 {
 und:'19',
 login:'MARIA IZABELA',
 pass: '2128',
-linkd: '"https://docs.google.com/spreadsheets/d/1CHtFRnfjHxnXq3zxcN9GbHF-Khed-EKBZ7pUozLD0jo/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1CHtFRnfjHxnXq3zxcN9GbHF-Khed-EKBZ7pUozLD0jo/edit?usp=sharing'
 },
 {
 und:'21',
 login:'Maria Mikaely ',
 pass: 'umasenha97',
-linkd: '""'
+linkd: ''
 },
 {
 und:'15',
 login:'MAYARA MARTINS ',
 pass: '1103',
-linkd: '""'
+linkd: ''
 },
 {
 und:'20',
 login:'Michael.D',
 pass: 'Benicio16',
-linkd: '""'
+linkd: ''
 },
 {
 und:'4',
 login:'Natalia N ',
 pass: '1307',
-linkd: '""'
+linkd: ''
 },
 {
 und:'3',
@@ -117,117 +117,118 @@ linkd: 'https://docs.google.com/spreadsheets/d/1hFg6y1XSz3NoAi5YaRf5N8zO-VoEC7WS
 und:'5',
 login:'Rayssa Jussara Pereira Gualberto',
 pass: '12345678',
-linkd: '""'
+linkd: ''
 },
 {
 und:'17',
 login:'Rita de cassia',
 pass: '12345',
-linkd: '""'
+linkd: ''
 },
 {
 und:'16',
 login:'Sheila.cristina',
 pass: '123',
-linkd: '""'
+linkd: ''
 },
 {
 und:'13',
 login:'Tallys Ricardo ',
 pass: 'Senha*123456',
-linkd: '""'
+linkd: ''
 },
 {
 und:'18',
 login:'Thaina Feitosa ',
 pass: '170220',
-linkd: '""'
+linkd: ''
 },
 {
 und:'18',
 login:'Vitória Rafaele ',
 pass: '2910',
-linkd: '""'
+linkd: ''
 },
 {
 und:'15',
 login:'VIVIAN SUERDA',
 pass: '2810',
-linkd: '""'
+linkd: ''
 },
 {
 und:'56',
 login:'ADRIANO',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1Tc6yuEGuVZdrbQhh382ncFE_e6EwfmjMROflcjLWvOo/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1Tc6yuEGuVZdrbQhh382ncFE_e6EwfmjMROflcjLWvOo/edit?usp=sharing'
 },
 {
 und:'11',
 login:'ALINE DAYENNE',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1N5xprvxi-aQHdmu_caPSNuMqsiLrU0yxGbb8g9Jes8E/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1N5xprvxi-aQHdmu_caPSNuMqsiLrU0yxGbb8g9Jes8E/edit?usp=sharing'
 },
 {
 und:'1',
 login:'CLAUDIONE',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1Vbuq8HdHcuT4ynCb8qZL-rXN96VcFORgEqVw3m7kUUM/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1Vbuq8HdHcuT4ynCb8qZL-rXN96VcFORgEqVw3m7kUUM/edit?usp=sharing'
 },
 {
 und:'3',
 login:'GRACIELE',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1CmnXjGgOnce_30hT9_zfzug62_PRNbYnAce0I-ztpl0/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1CmnXjGgOnce_30hT9_zfzug62_PRNbYnAce0I-ztpl0/edit?usp=sharing'
 },
 {
 und:'2',
 login:'JACKELINE',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1LmtxLMoW-eZUZ-3D8-FCIwbjiNYLdjDtz-ELj_JEMiA/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1LmtxLMoW-eZUZ-3D8-FCIwbjiNYLdjDtz-ELj_JEMiA/edit?usp=sharing'
 },
 {
 und:'7',
 login:'JOAO VITOR',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1k1oOq0q7H2ydjzjfdpXt6z_OfNVPbB8jmjA7OYHcAi0/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1k1oOq0q7H2ydjzjfdpXt6z_OfNVPbB8jmjA7OYHcAi0/edit?usp=sharing'
 },
 {
 und:'10',
 login:'JOSY',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/11yuMKVNjhmEoFfwLGPP1I6XFNSC-3fn_HnJl112sTsI/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/11yuMKVNjhmEoFfwLGPP1I6XFNSC-3fn_HnJl112sTsI/edit?usp=sharing'
 },
 {
 und:'7',
 login:'LEANDRA',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1k1oOq0q7H2ydjzjfdpXt6z_OfNVPbB8jmjA7OYHcAi0/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1k1oOq0q7H2ydjzjfdpXt6z_OfNVPbB8jmjA7OYHcAi0/edit?usp=sharing'
 },
 {
 und:'10',
 login:'LUCIA',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/13cHyjIBZI4SnfAjU68lJ-CY4p9FJsrIi9jLc0aoojiM/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/13cHyjIBZI4SnfAjU68lJ-CY4p9FJsrIi9jLc0aoojiM/edit?usp=sharing'
 },
 {
 und:'11',
 login:'MAYARA',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1qpWDa42eP7plxK1wT27MllaPCZ4BCG8k2GfZ6HjbCjE/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1qpWDa42eP7plxK1wT27MllaPCZ4BCG8k2GfZ6HjbCjE/edit?usp=sharing'
 },
 {
 und:'9',
 login:'NAYARA',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/1yDZnFG_oy_dE_UqPCbu9qG33GMwjywFW18Xrk5FGE2k/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/1yDZnFG_oy_dE_UqPCbu9qG33GMwjywFW18Xrk5FGE2k/edit?usp=sharing'
 },
 {
 und:'1',
 login:'TAWAN',
 pass: '123',
-linkd: '"https://docs.google.com/spreadsheets/d/18UOEmIT3G7jv8G-LQdbdBUte64_aH2Z1ok7tIXT1tc0/edit?usp=sharing"'
+linkd: 'https://docs.google.com/spreadsheets/d/18UOEmIT3G7jv8G-LQdbdBUte64_aH2Z1ok7tIXT1tc0/edit?usp=sharing'
 },
 ]
+
 
 
 
